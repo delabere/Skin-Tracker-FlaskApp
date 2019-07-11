909985f1924af1feb4e9a5f3adf00b0cb2dfb9f8
delabere <jack.rickards@hotmail.co.uk>
2019-07-11 22:52:40 +0100

    loading data successfully and passing it into the view function

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
-import os, random
+import os
+import random
+import json
 
 
 app = Flask(__name__)
@@ -8,16 +10,26 @@ app = Flask(__name__)
 # creates a list of hands from image filenames
 os.chdir(r'/Users/jackrickards/Documents/coding/python/CS50W/skin_tracker_app/static/images/')
 hands = [file for file in os.listdir() if 'jpg' in file]
+# read in stored hand_data
+with open(r'/Users/jackrickards/Documents/coding/python/CS50W/skin_tracker_app/storage.json') as json_file:
+    storage = json.load(json_file)
+
+def hand_picker(storage):
+    hands_data = [[file, storage[file]['position'], storage[file]['times_sorted']]
+                for file in storage]
+    random.shuffle(hands_data)
+    hands_data = (sorted(hands_data, key = lambda x: int(x[2])))
+    first, second = hands_data.pop(), hands_data.pop()
+    return [first, second]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    session_hands = random.sample(hands, 2)
+    session_hands = hand_picker(storage)
+    print(session_hands)
     if request.method == 'POST':
-        print(
-            f"The worse image was: {request.form['image_worse']} the better image was: {request.form['image_better']}")
-        print(f"The new progress is: {request.form['progress']}")
         progress = int(request.form['progress'])
         return render_template('index.html', progress=progress, session_hands=session_hands)
+
     else:
         progress = 0
         return render_template('index.html', progress=progress, session_hands=session_hands)
--- test.py
+++ test.py
@@ -1,20 +1,50 @@
 import json
 import os
+import random
 
+# create dictionary for initial hand_data
+# os.chdir(r'/Users/jackrickards/Documents/coding/python/CS50W/skin_tracker_app/static/images/')
+# hands = [file for file in os.listdir() if 'jpg' in file]
+#
+# storage = {}
+#
+# for index, hand in enumerate(hands, 1):
+#     storage[hand] = {'position': index, 'times_sorted': 0}
 
-os.chdir(r'/Users/jackrickards/Documents/coding/python/CS50W/skin_tracker_app/static/images/')
-hands = [file for file in os.listdir() if 'jpg' in file]
-
-storage = {}
-
-for index, hand in enumerate(hands, 1):
-    storage[hand] = {'position': index, 'times_sorted': 0}
-
-with open(r'/Users/jackrickards/Documents/coding/python/CS50W/skin_tracker_app/storage.json', 'w') as f:
-    json.dump(storage, f)
+# store data back into json
+# with open(r'/Users/jackrickards/Documents/coding/python/CS50W/skin_tracker_app/storage.json', 'w') as f:
+#     json.dump(storage, f)
 
 
 with open(r'/Users/jackrickards/Documents/coding/python/CS50W/skin_tracker_app/storage.json') as json_file:
     storage = json.load(json_file)
 
-print(storage)
+hands_data = [[file, storage[file]['position'], storage[file]['times_sorted']]
+            for file in storage]
+#
+# unsorted = [[file, storage[file]['position'], storage[file]['times_sorted']]
+#             for file in storage if storage[file]['times_sorted'] == 0]
+
+# prev_sorted = [[file, storage[file]['position'], storage[file]['times_sorted']]
+#                for file in storage if storage[file]['times_sorted'] > 0]
+#
+# prev_sorted = [['testfile_1.jpg', 21, 1], ['testfile_1.jpg', 22, 3], [
+#     'testfile_1.jpg', 23, 2], ['testfile_1.jpg', 24, 2]]
+
+random.shuffle(hands_data)
+hands_data = (sorted(hands_data, key = lambda x: int(x[2])))
+first, second = hands_data.pop(), hands_data.pop()
+return first, second
+
+# if len(unsorted) >= 2:
+#     random.shuffle(unsorted)
+#     first = unsorted.pop()
+#     second = unsorted.pop()
+# elif (len(unsorted) == 1) and (len(prev_sorted) >= 1):
+#     first = unsorted.pop()
+#     prev_sorted = (sorted(prev_sorted, key = lambda x: int(x[2])))
+#     second = prev_sorted.pop(0)
+# else:
+#     prev_sorted = (sorted(prev_sorted, key = lambda x: int(x[2])))
+#     first = prev_sorted.pop(0)
+#     second = prev_sorted.pop(0)
