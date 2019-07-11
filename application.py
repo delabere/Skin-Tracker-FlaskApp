0789eaae4e15b3c9cacd34c26d9a41c8445685f5
delabere <jack.rickards@hotmail.co.uk>
2019-07-11 15:51:10 +0100

    made progress bar dynamic

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    progress = 5
+    return render_template('index.html', progress=progress)
